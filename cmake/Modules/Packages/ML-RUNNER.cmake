d9e0754b62606b99c63afa4c7c5cdf1eed4c7727
aknoll <aknoll@tc2-pc007.theochem2.ruhr-uni-bochum.de>
2025-10-13 10:03:19 +0200

    Update build script with more meaningful options.

--- cmake/Modules/Packages/ML-RUNNER.cmake
+++ cmake/Modules/Packages/ML-RUNNER.cmake
@@ -1,18 +1,24 @@
 # Enable Fortran language support
 enable_language(Fortran)
 
-# Option to force downloading RuNNer even if found
-option(DOWNLOAD_RUNNER "Force download and build of RuNNer" OFF)
+# Build arguments.
+option(DOWNLOAD_RUNNER "Force download and build of RuNNer. If this is OFF\
+  the arguments RUNNER_LIB_DIR, RUNNER_LIB_NAME and RUNNER_SHARED_LIB must\
+  be set to meaningful values." OFF
+)
+option(RUNNER_SHARED_LIB "Use pre-compiled shared/dynamic RuNNer library. Only \
+  considered if DOWNLOAD_RUNNER is OFF." OFF
+)
+set(RUNNER_LIB_DIR "$ENV{HOME}/.local/lib" CACHE STRING "Root directory of a \
+  pre-compiled RuNNer installation. Only considered if DOWNLOAD_RUNNER is OFF."
+)
+set(RUNNER_LIB_NAME "libRuNNer" CACHE STRING "Name of the RuNNer library \
+  (excluding file extension, this is controlled by RUNNER_SHARED_LIB). Only \
+  considered if DOWNLOAD_RUNNER is OFF."
+)
 
-# Option to build RuNNer as a shared library
-option(BUILD_RUNNER_SHARED "Build RuNNer as a shared/dynamic library." OFF)
-
-# Try to find an existing installation of RuNNer
-find_package(RuNNer QUIET)
-
-# If RuNNer is not found or a download is forced, use ExternalProject
-if(NOT RuNNer_FOUND OR DOWNLOAD_RUNNER)
-  message(STATUS "RuNNer not found or download forced. Building from source.")
+if(DOWNLOAD_RUNNER)
+  message(STATUS "DOWNLOAD_RUNNER is ON. Building RuNNer from source as a static library.")
 
   # Include ExternalProject module
   include(ExternalProject)
@@ -21,22 +27,15 @@ if(NOT RuNNer_FOUND OR DOWNLOAD_RUNNER)
   set(RUNNER_CMAKE_ARGS
     -DUSE_MPI=ON
     -DCMAKE_Fortran_FLAGS="-fPIC"
-    -DUSE_SHARED_LIB=yes
     -DENABLE_TESTS=no
   )
 
-  # Set the library file name based on the user's choice
-  if(BUILD_RUNNER_SHARED)
-    set(RUNNER_LIB_NAME "libRuNNer.so")
-    message(STATUS "Will build RuNNer as a shared library.")
-  else()
-    set(RUNNER_LIB_NAME "libRuNNer.a")
-    message(STATUS "Will build RuNNer as a static library.")
-  endif()
+  # Force using the static library. RuNNer's cmake build always produces libRuNNer.a.
+  set(RUNNER_LIB_NAME "libRuNNer.a")
 
   ExternalProject_Add(runner_build
     GIT_REPOSITORY "git@gitlab.com:runner-suite/runner2.git"
-    GIT_TAG "build-cmake-shared"
+    GIT_TAG "main"
     GIT_SHALLOW YES
     GIT_PROGRESS YES
 
@@ -57,23 +56,57 @@ if(NOT RuNNer_FOUND OR DOWNLOAD_RUNNER)
     BUILD_BYPRODUCTS ${CMAKE_CURRENT_BINARY_DIR}/runner_install/RuNNer/lib/${RUNNER_LIB_NAME}
   )
 
-  # Get the installation directory of the external project
-  ExternalProject_Get_Property(runner_build INSTALL_DIR)
-
   # Create an IMPORTED library target for RuNNer
-  add_library(RuNNer::RuNNer UNKNOWN IMPORTED)
+  add_library(RuNNer::RuNNer STATIC IMPORTED)
   set_target_properties(RuNNer::RuNNer PROPERTIES
     IMPORTED_LOCATION "${CMAKE_CURRENT_BINARY_DIR}/runner_install/RuNNer/lib/${RUNNER_LIB_NAME}"
   )
 
-  # Add a dependency to ensure RuNNer is built before LAMMPS
+  # Add a dependency to ensure RuNNer is built before the main target
   add_dependencies(lammps runner_build)
 
-  # Link LAMMPS to the newly built RuNNer library
-  target_link_libraries(lammps PRIVATE RuNNer::RuNNer ${LAPACK_LIBRARIES} ${BLAS_LIBRARIES})
-
 else()
-  # If RuNNer is found, link to the existing library
-  message(STATUS "Found RuNNer: ${RuNNer_LIBRARIES}")
-  target_link_libraries(lammps PRIVATE RuNNer::RuNNer ${LAPACK_LIBRARIES} ${BLAS_LIBRARIES})
+  message(STATUS "DOWNLOAD_RUNNER is OFF. Looking for a pre-compiled RuNNer.")
+
+  # Check if the directory specified by RUNNER_LIB_DIR exists
+  if(NOT IS_DIRECTORY ${RUNNER_LIB_DIR})
+    message(FATAL_ERROR "The directory specified by RUNNER_LIB_DIR does not exist: ${RUNNER_LIB_DIR}")
+  endif()
+
+  # Determine the correct file extension based on whether the library is shared or not.
+  if(RUNNER_SHARED_LIB)
+    set(RUNNER_LIB_EXT ".so")
+    set(RUNNER_LIB_TYPE SHARED)
+  else()
+    set(RUNNER_LIB_EXT ".a")
+    set(RUNNER_LIB_TYPE STATIC)
+  endif()
+
+  set(RUNNER_LIB_FULL_NAME ${RUNNER_LIB_NAME}${RUNNER_LIB_EXT})
+  message(STATUS "Looking for the ${RUNNER_LIB_TYPE} library: ${RUNNER_LIB_FULL_NAME}")
+
+  # Find the RuNNer library in the specified path
+  find_library(RuNNer_LIBRARY
+    NAMES ${RUNNER_LIB_FULL_NAME}
+    HINTS "${RUNNER_LIB_DIR}"
+    NO_DEFAULT_PATH
+  )
+
+  if(RuNNer_LIBRARY)
+    message(STATUS "Found RuNNer library: ${RuNNer_LIBRARY}")
+
+    # Create an IMPORTED library target for the found RuNNer library
+    add_library(RuNNer::RuNNer ${RUNNER_LIB_TYPE} IMPORTED)
+    set_target_properties(RuNNer::RuNNer PROPERTIES
+      IMPORTED_LOCATION "${RuNNer_LIBRARY}"
+    )
+  else()
+    message(FATAL_ERROR "Could not find the RuNNer library in the specified \
+      RUNNER_LIB_DIR. Searched for ${RUNNER_LIB_FULL_NAME} at ${RUNNER_LIB_DIR}"
+    )
+  endif()
+
 endif()
+
+# Link lammps to the found RuNNer library
+target_link_libraries(lammps PRIVATE RuNNer::RuNNer ${LAPACK_LIBRARIES} ${BLAS_LIBRARIES})
